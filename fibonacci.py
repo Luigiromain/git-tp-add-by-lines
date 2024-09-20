86b52610c7c47ae848c24464ec26750f57392170
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2024-09-20 14:55:35 +0200

    Mise en place de choix de fonction

--- fibonacci.py
+++ fibonacci.py
@@ -7,6 +7,13 @@ la suite de fibonacci
 from functools import cache
 
 
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
+
 @cache
 def fibo_cached(n):
     """
--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from argparse import ArgumentParser
 
 # on importe le module de calcul
 from fibonacci import fibo_cached
+from fibonacci import fibo
 
 # acquisition du paramètre à partir de la ligne de commande
 parser = ArgumentParser()
 parser.add_argument("n", type=int)
+# on utilise 1 si on veut cacher, 0 sinon
+parser.add_argument("cache", type=int)
 args = parser.parse_args()
 n = args.n
 
+cache = args.cache
 # calcul et affichage
-print(f"fibo_cached({n}) = {fibo_cached(n)}")
+if cache == 1:
+    print(f"fibo_cached({n}) = {fibo_cached(n)}")
+else:
+    print(f"fibo({n}) = {fibo(n)}")
